10a59d09e1c807b73298097d458be96d8fd62f93
Slavikkq <yaroslavmanuljak@gmail.com>
2024-01-22 15:01:55 +0200

    js hw 09

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,26 +3,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
   form.addEventListener('input', event => {
     const formData = {
-      email: form.email.value,
-      message: form.message.value,
+      email: form.email.value.trim(),
+      message: form.message.value.trim(),
     };
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
   });
 
-  window.addEventListener('load', () => {
-    const storedFormData = localStorage.getItem('feedback-form-state');
-    if (storedFormData) {
-      const formData = JSON.parse(storedFormData);
-      form.email.value = formData.email;
-      form.message.value = formData.message;
-    }
-  });
+  const storedFormData = localStorage.getItem('feedback-form-state');
+  if (storedFormData) {
+    const formData = JSON.parse(storedFormData);
+    form.email.value = formData.email;
+    form.message.value = formData.message;
+  }
 
   form.addEventListener('submit', event => {
     event.preventDefault();
     const formData = {
-      email: form.email.value,
-      message: form.message.value,
+      email: form.email.value.trim(),
+      message: form.message.value.trim(),
     };
     console.log(formData);
     localStorage.removeItem('feedback-form-state');
